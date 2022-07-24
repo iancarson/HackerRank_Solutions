cf2630bb4808ef831ea8f84ce654618cc1883617
ian <38280903+iancarson@users.noreply.github.com>
2022-07-24 17:38:32 +0300

    Create First-IO.java

---
+++ HackerRank test/HackeRankChallenges/First-IO.java
@@ -0,0 +1,73 @@
+private static final FastReader scanner = new FastReader();
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            int n = scanner.nextInt();
+
+            int[] c = new int[n];
+
+            String[] cItems = scanner.nextLine().split(" ");
+
+            for (int i = 0; i < n; i++) {
+                int cItem = Integer.parseInt(cItems[i]);
+                c[i] = cItem;
+            }
+
+            int[][] edges = new int[n - 1][2];
+
+            for (int i = 0; i < n - 1; i++) {
+                String[] edgesRowItems = scanner.nextLine().split(" ");
+                
+                for (int j = 0; j < 2; j++) {
+                    int edgesItem = Integer.parseInt(edgesRowItems[j]);
+                    edges[i][j] = edgesItem;
+                }
+            }
+
+            int result = balancedForest(c, edges);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+    }
+    
+    static class FastReader { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() { 
+            while (st == null || !st.hasMoreElements()) { 
+                try { 
+                    st = new StringTokenizer(br.readLine()); 
+                } catch (IOException  e) { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        String nextLine() { 
+            String str = ""; 
+            try { 
+                str = br.readLine(); 
+            } catch (IOException e) { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    }
