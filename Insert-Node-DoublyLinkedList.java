b61b79470e8b32fd20373ece754ff7bdb4c4d4df
ian <38280903+iancarson@users.noreply.github.com>
2022-07-26 17:32:06 +0300

    Create Insert-Node-DoublyLinkedList.java

---
+++ Insert-Node-DoublyLinkedList.java
@@ -0,0 +1,108 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class DoublyLinkedListNode {
+        public int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
+
+        public DoublyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    static class DoublyLinkedList {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
+
+        public DoublyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+                node.prev = this.tail;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printDoublyLinkedList(DoublyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+
+
+    /*
+     * Complete the 'sortedInsert' function below.
+     *
+     * The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+     * The function accepts following parameters:
+     *  1. INTEGER_DOUBLY_LINKED_LIST llist
+     *  2. INTEGER data
+     */
+
+    /*
+     * For your reference:
+     *
+     * DoublyLinkedListNode {
+     *     int data;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
+     * }
+     *
+     */
+
+    public static DoublyLinkedListNode sortedInsert(DoublyLinkedListNode llist, int data) {
+    // Write your code here
+    DoublyLinkedListNode node = new DoublyLinkedListNode(data);
+    DoublyLinkedListNode currNode = llist;
+    if(currNode == null)
+    {
+        return node;
+    }
+    else if(data <= llist.data)
+    {
+        node.next = llist;
+        llist.prev = node;
+        return node;
+    }
+    else
+    {
+        DoublyLinkedListNode tempNode = sortedInsert(llist.next, data);
+        llist.next = tempNode;
+        tempNode.prev = llist;
+        return llist;
+    }
+    }
+    
+
+    
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
