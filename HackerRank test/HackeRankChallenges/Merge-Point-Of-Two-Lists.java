33ddff615cc784aaac0146f055ef6b6d71b4f4fe
ian <38280903+iancarson@users.noreply.github.com>
2022-07-27 13:58:33 +0300

    Create Merge-Point-Of-Two-Lists.java

---
+++ HackerRank test/HackeRankChallenges/Merge-Point-Of-Two-Lists.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the findMergeNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static int findMergeNode(SinglyLinkedListNode headA, SinglyLinkedListNode headB) {
+       SinglyLinkedListNode currentA= headA;
+       SinglyLinkedListNode currentB = headB;
+       while(currentA != currentB)
+       {
+           if(currentA == null)
+           {
+               currentA = headB;
+           }
+           else
+           {
+               currentA = currentA.next;
+           }
+           if(currentB == null)
+           {
+               currentB = headA;
+           }
+           else
+           {
+               currentB = currentB.next;
+           }
+       }
+       return currentB.data;  
+}
+
+    private static final Scanner scanner = new Scanner(System.in);
