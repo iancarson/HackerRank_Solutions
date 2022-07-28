9e0867a8a48f2af567d3880de11c73b5640d1f8c
ian <38280903+iancarson@users.noreply.github.com>
2022-07-28 14:03:19 +0300

    Create Flipping-Bits.py

---
+++ HackerRank test/HackeRankChallenges/Flipping-Bits.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def flippingBits(n):
+    # Write your code here
+    flipping = 4294967295;
+    return n ^ flipping;
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
